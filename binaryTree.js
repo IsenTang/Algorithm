cb091f15dbe21443ac2e76298dfff2387619ed97
System Administrator <root@tangyichaodeMacBook-Pro.local>
2018-01-03 16:41:30 +0800

    binaryTree

--- binaryTree.js
+++ binaryTree.js
@@ -40,7 +40,7 @@ module.exports = {
 
     },
 
-    //中序遍历
+    //中序遍历  从小到大排序
     InOrderTraversal: function(treeNode) {
         let arr = new Array();
 
@@ -62,12 +62,153 @@ module.exports = {
             arr.push(key);
         };
 
-        this.result = () => {
-            return arr;
-        };
+        this.result = () => arr;
 
 
         orderTraversal(treeNode, cb);
+    },
+
+    //前序遍历 复制 效率高。
+    PreOrderTraversal: function(treeNode) {
+        let node = {};
+        const copyTraversal = (treeNode, node) => {
+            preCopyTraversal(treeNode, node);
+        };
+
+        const preCopyTraversal = (treeNode, node) => {
+            if (treeNode !== null) {
+                node.key = treeNode.key;
+                if (treeNode.left !== null) {
+                    node.left = {};
+                    node.left.key = treeNode.left.key;
+                    preCopyTraversal(treeNode.left, node.left);
+                } else {
+                    node.left = null;
+                }
+
+                if (treeNode.right !== null) {
+                    node.right = {};
+                    node.right.key = treeNode.right.key;
+                    preCopyTraversal(treeNode.right, node.right);
+                } else {
+                    node.right = null;
+                }
+            }
+        };
+
+        this.result = () => node;
+
+
+        copyTraversal(treeNode, node);
+    },
+
+    //后序遍历 操作系统文件遍历
+    PostOrderTraversal: function(treeNode) {
+        let arr = new Array();
+        const orderTraversal = (treeNode, cb) => {
+            postOrderTraversal(treeNode, cb);
+        };
+
+        const postOrderTraversal = (treeNode, cb) => {
+            if (treeNode !== null) {
+                postOrderTraversal(treeNode.left, cb);
+                postOrderTraversal(treeNode.right, cb);
+                cb(treeNode.key);
+            }
+        };
+
+        let cb = (key) => {
+            arr.push(key);
+        };
+
+        orderTraversal(treeNode, cb);
+        this.result = () => arr;
+    },
+
+    max: (treeNode) => {
+        let result = 0;
+        const maxTreeNode = (treeNode) => {
+            getMaxTreeNode(treeNode);
+        };
+
+        const getMaxTreeNode = (treeNode) => {
+            if (treeNode !== null) {
+                if (treeNode.right !== null) {
+                    getMaxTreeNode(treeNode.right);
+                } else {
+                    result = treeNode.key;
+                }
+            }
+        };
+
+        maxTreeNode(treeNode);
+        return result;
+    },
+    min: (treeNode) => {
+        let result = 0;
+        const minTreeNode = (treeNode) => {
+            getMinTreeNode(treeNode);
+        };
+
+        const getMinTreeNode = (treeNode) => {
+            if (treeNode !== null) {
+                if (treeNode.left !== null) {
+                    getMinTreeNode(treeNode.left);
+                } else {
+                    result = treeNode.key;
+                }
+            }
+        };
+
+        minTreeNode(treeNode);
+        return result;
+    },
+    getIndex: (treeNode, num) => {
+        let result = {};
+        if (treeNode === null) {
+            result.state = false;
+            return result;
+        }
+
+        const index = (treeNode, num) => {
+            if (num === treeNode.key) {
+                result.state = true;
+                result.num = num;
+            } else {
+                if (num > treeNode.key) {
+                    if (treeNode.right !== null)
+                        index(treeNode.right, num);
+                    else {
+                        result.state = false;
+                        result.num = num;
+                    }
+                } else {
+                    if (treeNode.left !== null)
+                        index(treeNode.left, num);
+                    else {
+                        result.state = false;
+                        result.num = num;
+                    }
+                }
+            }
+        };
+
+        index(treeNode, num);
+        return result;
+    },
+    remove: (treeNode, num) => {
+        let result = {};
+        if (treeNode === null) {
+            result.state = false;
+            return result;
+        }
+
+        // const indexAndRemove = (treeNode, num) => {
+
+        // };
+
+        // indexAndRemove(treeNode, num);
+        // return result;
     }
 
 };
--- binaryTree.test.js
+++ binaryTree.test.js
@@ -1,5 +1,10 @@
 const BinaryTree = require('./binaryTree').BinaryTree;
 const InOrderTraversal = require('./binaryTree').InOrderTraversal;
+const PreOrderTraversal = require('./binaryTree').PreOrderTraversal;
+const PostOrderTraversal = require('./binaryTree').PostOrderTraversal;
+const max = require('./binaryTree').max;
+const min = require('./binaryTree').min;
+const getIndex = require('./binaryTree').getIndex;
 const chai = require('chai');
 const assertArrays = require('chai-arrays');
 chai.use(assertArrays);
@@ -8,16 +13,16 @@ chai.use(assertArrays);
 let expect = chai.expect;
 
 //中序遍历
-describe('binary tree test', function() {
+describe('binaryTree inOrder traversal test', function() {
     it('result should be [1,2,3,4,5,6]', function() {
         expect(
-            test()
+            inOrderTraversal()
         ).to.be.equalTo([1, 2, 3, 4, 5, 6]);
     });
 });
 
 
-function test() {
+function inOrderTraversal() {
     let arr = [2, 4, 3, 1, 6, 5];
     let binaryTree = new BinaryTree();
     let root = null;
@@ -26,4 +31,122 @@ function test() {
     }
     let traversal = new InOrderTraversal(root);
     return traversal.result();
+}
+
+//前序排序
+describe('binaryTree  preOrder traversal test', function() {
+    it('', function() {
+        expect(
+            preOrderTraversal()
+        ).to.deep.equal(obj);
+    });
+});
+
+function preOrderTraversal() {
+    let arr = [2, 4, 3, 1, 6, 5];
+    let binaryTree = new BinaryTree();
+    let root = null;
+    for (let i = 0; i < arr.length; i++) {
+        root = binaryTree.insert(arr[i]);
+    }
+    let traversal = new PreOrderTraversal(root);
+    return traversal.result();
+}
+
+const obj = {
+    key: 2,
+    left: { key: 1, left: null, right: null },
+    right: {
+        key: 4,
+        left: { key: 3, left: null, right: null },
+        right: {
+            key: 6,
+            left: {
+                key: 5,
+                left: null,
+                right: null
+            },
+            right: null
+        }
+    }
+};
+
+
+//后序排序
+describe('binaryTree  postOrder traversal test', function() {
+    it('', function() {
+        expect(
+            postOrderTraversal()
+        ).to.be.equalTo([1, 3, 5, 6, 4, 2]);
+    });
+});
+
+function postOrderTraversal() {
+    let arr = [2, 4, 3, 1, 6, 5];
+    let binaryTree = new BinaryTree();
+    let root = null;
+    for (let i = 0; i < arr.length; i++) {
+        root = binaryTree.insert(arr[i]);
+    }
+    let traversal = new PostOrderTraversal(root);
+    return traversal.result();
+}
+
+
+//最大值
+describe('binaryTree  get max test', function() {
+    it('max should be 6', function() {
+        expect(
+            getMax()
+        ).to.be.equal(6);
+    });
+});
+
+function getMax() {
+    let arr = [2, 4, 3, 1, 6, 5];
+    let binaryTree = new BinaryTree();
+    let root = null;
+    for (let i = 0; i < arr.length; i++) {
+        root = binaryTree.insert(arr[i]);
+    }
+    return max(root);
+}
+
+//最小值
+describe('binaryTree  get min test', function() {
+    it('min should be 1', function() {
+        expect(
+            getMin()
+        ).to.be.equal(1);
+    });
+});
+
+function getMin() {
+    let arr = [2, 4, 3, 1, 6, 5];
+    let binaryTree = new BinaryTree();
+    let root = null;
+    for (let i = 0; i < arr.length; i++) {
+        root = binaryTree.insert(arr[i]);
+    }
+    return min(root);
+}
+
+
+//查找
+describe('binaryTree  get index test', function() {
+    it('if exist should return true', function() {
+        expect(
+            index()
+        ).to.deep.equal({ state: true, num: 6 });
+    });
+});
+
+function index() {
+    let arr = [2, 4, 3, 1, 6, 5];
+    let binaryTree = new BinaryTree();
+    let root = null;
+    for (let i = 0; i < arr.length; i++) {
+        root = binaryTree.insert(arr[i]);
+    }
+    return getIndex(root, 6);
 }
